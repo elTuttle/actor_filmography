02e9e8ad7d03fa8817e747f4e3d35b1b8c793526
John Tuttle <thejwt@gmail.com>
2017-10-07 22:33:28 +0000

    scraper update

--- six-degrees/lib/scraper.rb
+++ six-degrees/lib/scraper.rb
@@ -6,7 +6,12 @@ class Scraper
 
   def self.scrape_actor_page(actor_url)
     doc = Nokogiri::HTML(open(actor_url))
-
+    #binding.pry
+    temp_array = []
+    doc.css("#filmographyTbl .unstyled").each do |film|
+      temp_array << film.text
+    end
+    temp_array
   end
 
   def self.scrape_film_page(film_url)
@@ -17,3 +22,4 @@ end
 
 
 ben_kingsley_films = Scraper.scrape_actor_page("https://www.rottentomatoes.com/celebrity/ben_kingsley")
+puts ben_kingsley_films
