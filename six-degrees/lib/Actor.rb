8e11a455b696561488b9fc8ffdbb1a77f78aebdb
John Tuttle <thejwt@gmail.com>
2017-10-07 22:02:44 +0000

    actor, film, and scraper class created

---
+++ six-degrees/lib/Actor.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Actor
+
+  attr_reader :name
+  attr_accessor :filmography
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
---
+++ six-degrees/lib/Film.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Film
+
+  attr_reader :name
+  attr_accessor :actors
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
