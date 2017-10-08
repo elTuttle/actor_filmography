2a7246c7c3c31e7a1d281d26c65e63360f7e1a65
John Tuttle <thejwt@gmail.com>
2017-10-08 05:32:46 +0000

    scrape_film_page finished

--- six-degrees/lib/scraper.rb
+++ six-degrees/lib/scraper.rb
@@ -8,23 +8,37 @@ class Scraper
     actor = actor.split(" ")
     actor = actor.join("_")
     actor = actor.downcase
-    binding.pry
+
     actor_url = "https://www.rottentomatoes.com/celebrity/" + actor
     doc = Nokogiri::HTML(open(actor_url))
-    #binding.pry
+
     temp_array = []
+
     doc.css("#filmographyTbl .unstyled").each do |film|
       temp_array << film.text
     end
+
     temp_array
   end
 
   def self.scrape_film_page(film_url)
+    doc = Nokogiri::HTML(open(film_url))
 
+    temp_array = []
+
+    doc.css(".castSection .unstyled span").each do |actor|
+      actor_name = actor.text
+      actor_name = actor_name.split(" ")
+      actor_name = actor_name.join(" ")
+      temp_array << actor_name
+    end
+    temp_array
   end
 
 end
 
 
 films = Scraper.scrape_actor_page("Johnny Depp")
-puts films
+actors = Scraper.scrape_film_page("https://www.rottentomatoes.com/m/blade_runner_2049")
+
+puts actors
