c0b192b05011ac82016e867a9a5198052867de77
John Tuttle <thejwt@gmail.com>
2017-10-08 04:23:46 +0000

    scraper now able to format actor name

--- six-degrees/lib/scraper.rb
+++ six-degrees/lib/scraper.rb
@@ -4,7 +4,12 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_actor_page(actor_url)
+  def self.scrape_actor_page(actor)
+    actor = actor.split(" ")
+    actor = actor.join("_")
+    actor = actor.downcase
+    binding.pry
+    actor_url = "https://www.rottentomatoes.com/celebrity/" + actor
     doc = Nokogiri::HTML(open(actor_url))
     #binding.pry
     temp_array = []
@@ -21,5 +26,5 @@ class Scraper
 end
 
 
-ben_kingsley_films = Scraper.scrape_actor_page("https://www.rottentomatoes.com/celebrity/ben_kingsley")
-puts ben_kingsley_films
+films = Scraper.scrape_actor_page("Johnny Depp")
+puts films
